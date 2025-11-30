7c7d4b7c7d3e298ae11591a91cafd9793d6de5d9
Malik <31506138+msmalik681@users.noreply.github.com>
2025-11-30 16:28:37 +0000

    Revert "auto commit"
    putting back everything that was deleted by accident.

---
+++ Shadow_Dancer/Blocks.tsx
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="blocks" tilewidth="16" tileheight="16" tilecount="256" columns="0">
+  <image source="blocks.png" width="256" height="256" />
+  <tile id="0">
+    <properties>
+      <property name="Name" value="lower_block" type="string" />
+    </properties>
+  </tile>
+  <tile id="1">
+    <properties>
+      <property name="Name" value="upper_block" type="string" />
+    </properties>
+  </tile>
+  <tile id="2">
+    <properties>
+      <property name="Name" value="jump_up" type="string" />
+    </properties>
+  </tile>
+  <tile id="3">
+    <properties>
+      <property name="Name" value="block_enemy_jump" type="string" />
+    </properties>
+  </tile>
+  <tile id="4">
+    <properties>
+      <property name="Name" value="jump_down" type="string" />
+    </properties>
+  </tile>
+  <tile id="5">
+    <properties>
+      <property name="Name" value="jump_up_and_down" type="string" />
+    </properties>
+  </tile>
+  <tile id="6">
+    <properties>
+      <property name="Name" value="block_pitfall" type="string" />
+    </properties>
+  </tile>
+</tileset>
---
+++ Shadow_Dancer/audio/readme.txt
@@ -0,0 +1,1 @@
+audio samples can be speeded up with audacity effect then to 8svx using: wav28svx in.wav out.8svx
